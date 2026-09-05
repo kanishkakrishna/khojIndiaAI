6209d8164ad857eb79de83cf139ddc6b429a2c12
kanishka <kanishkakrishna694@gmail.com>
2026-09-05 06:24:03 +0530

    removed ai agent

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 # Dono routers ko ek saath import karo
 from places import router as places_router
-from agent import router as agent_router
 
 app = FastAPI(title="KhojIndia API Engine")
 
@@ -23,7 +22,6 @@ app.add_middleware(
 # =====================================================================
 # Sabhi connections yahan se honge
 app.include_router(places_router, tags=["Places"])
-app.include_router(agent_router, tags=["Agent"])
 
 @app.get("/")
 def home():
